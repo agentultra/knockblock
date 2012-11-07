98093000d7b10e70949931702973e8b62f3a1b77
James King <james@agentultra.com>
2012-11-06 19:50:34 -0500

    Add method documentation to Collection
    Just adding some basic doc string documentation for posterity.

--- knockblock/collections/base.py
+++ knockblock/collections/base.py
@@ -15,6 +15,12 @@ class Collection(object):
         self._storage = {}
 
     def insert(self, tup):
+        """
+        Insert a new fact into the collection.
+
+        :param tup: An iterable object whose values should correspond
+                    to the given schema of the collection.
+        """
         fact = self._struct(*tup)
         key = tuple([attrgetter(k)(fact) for k in self.key_columns])
         self._storage[key] = fact
@@ -23,8 +29,23 @@ class Collection(object):
         return self._storage.values()
 
     def _project(self, func):
+        """
+        Return a list of tuples.
+
+        The function passed to _project receives a single fact from
+        the collection and its return value is wrapped by tuple().
+
+        :param func: A function taking a single fact as its parameter
+                     and returning an iterable.
+        :return: A list of tuples.
+        """
         return [tuple(func(t)) for t in self._storage.values() if
                 t is not None]
 
     def __getitem__(self, key):
+        """
+        Return a fact from the collection.
+
+        :param key: A tuple of values that form the key of the fact.
+        """
         return self._storage.get(key)
